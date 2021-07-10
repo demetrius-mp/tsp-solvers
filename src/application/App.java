3e0036716171cd54878c60bbfd49b61dcfa36311
Demetrius <demetrius.mp789@gmail.com>
2021-07-10 04:32:06 -0400

    Implementando utilitarios

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,24 @@
 package application;
+
+import utils.Solution;
+import utils.Vertex;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Vertex v1 = new Vertex(1, 1, 1);
+        Vertex v2 = new Vertex(2, 2, 2);
+
+        System.out.println("Test 1: Building solution with path size");
+        Solution s1 = new Solution(2);
+        s1.addVertex(v1);
+        s1.addVertex(v2);
+        System.out.println(s1);
+
+        System.out.println();
+
+        System.out.println("Test 2: Building solution with array of vertexes");
+        Vertex[] path = {v1, v2};
+        s1 = new Solution(path);
+        System.out.println(s1);
     }
 }
--- src/utils/Solution.java
+++ src/utils/Solution.java
@@ -3,6 +3,7 @@ package utils;
 public class Solution {
     public Vertex[] path;
     public double distance;
+    private int i;
 
     public Solution() {}
     
@@ -13,9 +14,40 @@ public class Solution {
 
     public Solution(Vertex[] path) {
         this.path = path;
+        this.distance = Solution.getDistance(path);
     }
 
     public Solution(int pathSize) {
         this.path = new Vertex[pathSize];
+        this.i = 0;
+        this.distance = 0;
     }
+
+    public void addVertex(Vertex v) {
+        if (this.i == this.path.length) {
+            throw new IndexOutOfBoundsException("Array is already full");
+        }
+
+        if (this.i != 0) {
+            this.distance += this.path[this.i - 1].distanceFrom(v);
+        }
+
+        this.path[this.i] = v;
+        this.i++;
+    }
+
+    public static double getDistance(Vertex[] path) {
+        double distance = 0;
+        for (int i = 0; i < path.length - 1; i++) {
+            distance += path[i].distanceFrom(path[i + 1]);
+        }
+
+        return distance;
+    }
+
+    @Override
+    public String toString() {
+        return "Solution [distance=" + distance + "]";
+    }
+
 }
